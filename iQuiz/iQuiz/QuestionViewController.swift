0d14452a8d15c7227475f6644f1343b23e395b5e
Amandha W. Barok <amandha.barok@gmail.com>
2023-06-06 17:06:26 -0400

    Add QuestionViewController

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -9,13 +9,55 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var points = 0
+    var questionNumber = 0
+    
+    @IBOutlet weak var questionTitleLabel: UILabel!
+    @IBOutlet var responseButtonList: [UIButton]!
+    
+    @IBAction func responseButtonPressed(_ sender: UIButton) {
+        let userGotRight = questions[questionNumber].correctResponse == sender.tag
+        
+        if userGotRight {
+            points += 1
+            sender.backgroundColor = UIColor(red: 11/225, green: 161/255, blue: 53/255, alpha: 1.0)
+        } else {
+            sender.backgroundColor = UIColor(red: 211/225, green: 17/255, blue: 17/255, alpha: 1.0)
 
-        // Do any additional setup after loading the view.
+        }
+        
+        if questionNumber < questions.count - 1 {
+            questionNumber += 1
+            Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(configureQuestion), userInfo: nil, repeats: false)
+        }
     }
     
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLayout()
+        configureQuestion()
+    }
+    
+    func configureLayout() {
+        navigationItem.hidesBackButton = true
+        questionTitleLabel.numberOfLines = 0
+        questionTitleLabel.textAlignment = .center
+        for button in responseButtonList {
+            button.layer.cornerRadius = 12.0
+        }
+    }
+    
+    @objc func configureQuestion() {
+        questionTitleLabel.text = questions[questionNumber].title
+        
+        for button in responseButtonList {
+            let buttonTitle = questions[questionNumber].responses[button.tag]
+            
+            button.setTitle(buttonTitle, for: .normal)
+            button.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+        }
+    }
     /*
     // MARK: - Navigation
 
